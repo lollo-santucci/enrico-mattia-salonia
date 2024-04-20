b6aefa737ef46ee93471731b499064c47ecd02a6
Lorenzo Santucci <lorenzosantucci@hotmail.it>
2024-04-20 16:56:14 +0200

    add link to availability

--- src/components/atoms/Abstract/Abstract.tsx
+++ src/components/atoms/Abstract/Abstract.tsx
@@ -1,16 +1,18 @@
+import Link from "../Link/Link";
 import "./Abstract.style.css"
 
 interface AbstractProps {
     content: string;
     availability?: string;
+    link?: string;
 }
 
-const Abstract: React.FC<AbstractProps> = ({ content, availability = "upon request" }) => {
+const Abstract: React.FC<AbstractProps> = ({ content, availability = "Preliminary draft available upon request", link = "mailto:mattia.salonia1@gmail.com" }) => {
     return (
         <div className="abstract">
             <h5 className="abstract-title">Abstract</h5>
             <p className="abstract-content justify">{content}</p>
-            <p className="abstract-availability">[Preliminary draft available {availability}]</p>
+            <p className="abstract-availability">{<Link text={availability} href={link} />}</p>
         </div>
     );
 };
