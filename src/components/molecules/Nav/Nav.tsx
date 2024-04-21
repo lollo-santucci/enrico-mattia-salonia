0795bb182500519b2f25a3aca0a593ae82a80b67
Lorenzo Santucci <lorenzosantucci@hotmail.it>
2024-04-21 11:15:17 +0200

    add responsiveness to header and nav

--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -3,13 +3,17 @@ import NavItem from "../../atoms/NavItem/NavItem";
 import { usePathname } from "next/navigation";
 import "./Nav.style.css";
 
-const Nav: React.FC = () => {
+interface NavProps {
+    dir?: string;
+}
+
+const Nav: React.FC<NavProps> = ({ dir }) => {
     const pathname = usePathname();
     // create a nav item responsive, when the screen is small, the nav item will be hidden and replaced with an hamburger icon thath open a modal with the nav items
     
 
     return (
-        <nav className="nav">
+        <nav className={ "nav " + dir }>
             <NavItem text="Home" href="/" active={pathname === "/"} />
             <NavItem text="Research" href="/research" active={pathname === "/research"} />
             <NavItem text="Teaching" href="/teaching" active={pathname === "/teaching"} />
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,13 +1,39 @@
+"use client";
 import Logo from "../../atoms/Logo/Logo";
 import Nav from "../../molecules/Nav/Nav";
+import IconButton from "@/components/atoms/IconButton/IconButton";
+import Card from "@/components/atoms/Card/Card";
+import React, { useState } from 'react';
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 import "./Header.style.css";
 
 const Header: React.FC = () => {
+  const { width } = useWindowDimensions();
+  const navClass = width && width < 800 ? "closed" : "";
+  const iconClass = width && width < 800 ? "" : "closed";
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
+
   return (
-    <header className="header">
-      <Logo />
-      <Nav />
-    </header>
+    <section className="menu">
+      <header className="header">
+        <Logo />
+        <div>
+          <div className={navClass}>
+            <Nav />
+          </div>
+          <div className={iconClass}>
+            <IconButton icon={isOpen ? "close" : "hamburger"} onClick={toggleOpen}/>
+          </div>
+        </div>
+      </header>
+      <div className= {isOpen ? "menu-open" : "closed"}>
+        <Card>
+          <Nav dir="col"/>
+        </Card>
+      </div>
+    </section>
   );
 };
 
